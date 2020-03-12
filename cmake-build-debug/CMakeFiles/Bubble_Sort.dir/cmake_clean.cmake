822d76687fb8c87bc8e5616fc2649ca35f0b9ce0
brainywayne <eshunfrancisboateng@gmail.com>
2020-03-12 14:06:55 +0000

    Working code

---
+++ cmake-build-debug/CMakeFiles/Bubble_Sort.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "Bubble_Sort"
+  "Bubble_Sort.pdb"
+  "CMakeFiles/Bubble_Sort.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Bubble_Sort.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
